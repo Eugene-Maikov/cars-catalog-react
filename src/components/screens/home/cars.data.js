675e84792bf0ab68c92ce8d1524ee6c4bde7c44f
Eugene Maikov <61806051+Eugene-Maikov@users.noreply.github.com>
2023-06-13 21:18:03 +0300

    init: start project
    1. Display a list of cars on the screen (components)
    2. Add new cars to the list (useState)

---
+++ src/components/screens/home/cars.data.js
@@ -0,0 +1,20 @@
+export const cars = [
+  {
+    id: 1,
+    name: 'Toyota Camry',
+    price: 24970,
+    image: 'https://i.pinimg.com/originals/ae/2c/d4/ae2cd40a7622a337667e2fd8d232c129.jpg'
+  },
+  {
+    id: 2,
+    name: 'Honda Civic',
+    price: 22545,
+    image: 'https://cdn.carbuzz.com/gallery-images/2019-honda-civic-si-coupe-front-view-carbuzz-479093-1600.jpg'
+  },
+  {
+    id: 3,
+    name: 'Honda Civic2',
+    price: 15545,
+    image: 'https://cdn.carbuzz.com/gallery-images/2019-honda-civic-si-coupe-front-view-carbuzz-479093-1600.jpg'
+  }
+]
---
+++ src/components/screens/home/home.jsx
@@ -0,0 +1,29 @@
+import {cars as carsData } from './cars.data.js'
+import CarItem from "./cat-item/CarItem.jsx";
+import CreateCarForm from "./create-car-form/CreateCarForm.jsx";
+import {useState} from "react";
+
+function Home() {
+
+  const [cars, setCars] = useState(carsData)
+  console.log(cars)
+
+
+  return (
+    <div>
+      <h1>Cars catalog</h1>
+
+      <CreateCarForm setCars={setCars}/>
+
+      <div>
+        {cars.length ? cars.map(
+          car => <CarItem key={car.id} car={car}/>
+        ) : (
+          <p>There are not cars </p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Home
