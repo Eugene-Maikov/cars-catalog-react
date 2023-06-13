a3ee86ec9a8a6266ef5d8a949bd84e1e6bdae445
Eugene-Maikov <eugene-maikov2000@yandex.ru>
2023-06-13 21:52:35 +0300

    feat: add clear fields

--- src/components/screens/home/create-car-form/CreateCarForm.jsx
+++ src/components/screens/home/create-car-form/CreateCarForm.jsx
@@ -1,23 +1,43 @@
 import styles from './CreateCarForm.module.css'
 import {useState} from "react";
 
+const clearData = {
+  name: '',
+  price: '',
+  image: '',
+}
+
 // eslint-disable-next-line react/prop-types
 const CreateCarForm = ({setCars}) => {
 
-  const [name, setName] = useState('')
-  const [price, setPrice] = useState('')
-  const [image, setImage] = useState('')
+  const [data, setData] = useState(clearData)
 
   const createCar = (e) => {
     e.preventDefault()
-    setCars(prev => [{id: prev.length + 1, name, price, image}, ...prev ])
+    setCars(prev => [{id: prev.length + 1, ...data}, ...prev])
+
+    setData(clearData)
   }
 
   return (
     <form action="/" className={styles.form}>
-      <input placeholder='Name' onChange={e => setName(e.target.value)} value={name}/>
-      <input placeholder='Price' onChange={e => setPrice(e.target.value)} value={price}/>
-      <input placeholder='Image' onChange={e => setImage(e.target.value)} value={image}/>
+      <input
+        placeholder='Name'
+        onChange={e => setData(prev => ({
+          ...prev, name: e.target.value
+        }))} value={data.name}/>
+      <input
+        placeholder='Price'
+        onChange={e => setData(prev => ({
+          ...prev, price: e.target.value
+        }))}
+        value={data.price}/>
+      <input
+        placeholder='Image'
+        onChange={e => setData(prev => ({
+          ...prev, image: e.target.value
+        }))}
+        value={data.image}/>
       <button className='btn' onClick={e => createCar(e)}>create</button>
     </form>
   )
