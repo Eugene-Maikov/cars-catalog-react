a1a9fb5f9b3f734620210a3de45479eda02b7d7e
Eugene-Maikov <eugene-maikov2000@yandex.ru>
2023-06-14 23:37:01 +0300

    feat: started routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/styles/global.css'
+import Router from './components/Router.jsx';
 import Home from "./components/screens/home/home.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Home/>
+    <React.StrictMode>
+      <Router/>
+    </React.StrictMode>
 )
