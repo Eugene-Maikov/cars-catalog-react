efcaf36522802dde7a7b182197d347b2c522faa7
Eugene-Maikov <eugene-maikov2000@yandex.ru>
2023-06-16 08:13:56 +0300

    feat: validation form
    validation using react-hook-form

--- src/components/screens/home/create-car-form/CreateCarForm.jsx
+++ src/components/screens/home/create-car-form/CreateCarForm.jsx
@@ -1,44 +1,41 @@
 import styles from './CreateCarForm.module.css'
 import {useState} from "react";
-
-const clearData = {
-  name: '',
-  price: '',
-  image: '',
-}
+import {useForm} from "react-hook-form";
 
 // eslint-disable-next-line react/prop-types
 const CreateCarForm = ({setCars}) => {
 
   const [data, setData] = useState(clearData)
 
-  const createCar = (e) => {
-    e.preventDefault()
+  const {register, reset, handleSubmit, formState: {errors}} = useForm({
+    mode: 'onChange'
+  })
+
+  const createCar = data => {
+    console.log(data)
     setCars(prev => [{id: prev.length + 1, ...data}, ...prev])
 
     setData(clearData)
   }
 
   return (
-    <form action="/" className={styles.form}>
+    <form action="/" className={styles.form} onSubmit={handleSubmit(createCar)}>
       <input
+        {...register('name', { required:'Name is required' })}
         placeholder='Name'
-        onChange={e => setData(prev => ({
-          ...prev, name: e.target.value
-        }))} value={data.name}/>
+        />
+      {errors?.name?.message && <p style={
+        {color: 'red',}
+      }>Name is required</p>}
       <input
+        {...register('price', { register:true })}
         placeholder='Price'
-        onChange={e => setData(prev => ({
-          ...prev, price: e.target.value
-        }))}
-        value={data.price}/>
+      />
       <input
+        {...register('image', { register:true })}
         placeholder='Image'
-        onChange={e => setData(prev => ({
-          ...prev, image: e.target.value
-        }))}
-        value={data.image}/>
-      <button className='btn' onClick={e => createCar(e)}>create</button>
+      />
+      <button className='btn'>create</button>
     </form>
   )
 
